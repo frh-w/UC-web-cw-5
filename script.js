78a165b051e34245d5c79164a68dd8fba4382e45
frh-w <fwaleed001@gmail.com>
2022-07-07 21:02:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,30 @@ function BMI(h = 150, w = 52) {
   return result;
 }
 console.log(BMI(160, 55));
-
-function Status_Body(result) {
-  if (result < 18.5) {
-    status_Body_result = "You are underweight.";
-  } else if (result >= 18.5 && result < 25) {
-    status_Body_result = "You have a healthy weight.";
-  } else if (result >= 25) {
-    status_Body_result = "You are overweight";
-  }
-  return status_Body_result;
+function calculate(w = 52, h = 150) {
+  let desc = BMI(h, w);
+  return desc;
 }
-console.log(status_Body_result);
+
+function Status_Body(bmi) {
+  if (bmi < 18.5) {
+    status_Body_bmi = "You are underweight.";
+  } else if (bmi >= 18.5 && bmi < 25) {
+    status_Body_bmi = "You have a healthy weight.";
+  } else if (bmi >= 25) {
+    status_Body_bmi = "You are overweight";
+  }
+  return status_Body_bmi;
+}
+console.log(status_Body_bmi);
+function BMI(h = 150, w = 52) {
+  let result = w / (h * h);
+  return result;
+  let bmi = w / (h * h);
+  return bmi;
+}
+console.log(BMI(160, 55));
+function calculate(w = 52, h = 150) {
+  let bmi = BMI(h, w);
+  return bmi;
+}
